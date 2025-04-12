254c27fac53079f62cd470b67650e6d0b13b16d2
Abdulrahman Haramain <officialtechwithmano@gmail.com>
2025-04-12 04:27:10 +0300

    Update: Favicon configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icon.ico" />
+        <link rel="icon" type="image/x-icon" href="/icon.ico" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
